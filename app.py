667e887ecdaf0b52fe6c3f1265cbf15f43cbb348
Greg <gkmarsella@gmail.com>
2017-01-06 14:17:43 -0800

    added timer after each wildcard

--- app.py
+++ app.py
@@ -423,6 +423,7 @@ def results():
         print(tracks)
         if tracks['tracks']['items']:
             uri_list.append(tracks['tracks']['items'][0]['uri'])
+            time.sleep(0.5)
 
     track_uris = ','.join(uri_list)
     track_string = track_uris.replace(':', '%3A')
