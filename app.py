c9464057f6c5ba1af024b002ce4dac1ea65df0f1
Greg <gkmarsella@gmail.com>
2017-01-23 13:10:51 -0800

    added wildcard counter

--- app.py
+++ app.py
@@ -476,7 +476,7 @@ def get_tracks():
     if 'tracks' in name and (len(name['tracks'])) > 0:
         if name['tracks'].get('items') is not None and len(name['tracks']['items']) > 0 and name['tracks']['items'][0].get('id') is not None:
             add_song(playlist_id, name['tracks']['items'][0]['id'])
-            time.sleep(1.00)
+            
 
 
     spotify_player_source = "https://embed.spotify.com/?uri=spotify%3Auser%3A" + user_id + "%3Aplaylist%3A{}".format(quote(playlist_id))
