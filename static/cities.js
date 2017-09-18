41cfafb04a09f32ffdf7ad2a6a29e8a539054e2e
Greg <gkmarsella@gmail.com>
2017-09-18 01:47:00 -0700

    updated homescreen. Simplified search page to just showing cities. Advanced search is now needed for adding radius and date

--- static/cities.js
+++ static/cities.js
@@ -29766,20 +29766,19 @@ var states = new Bloodhound({
   datumTokenizer: Bloodhound.tokenizers.whitespace,
   queryTokenizer: Bloodhound.tokenizers.whitespace,
   // `states` is an array of state names defined in "The Basics"
-  local: states,
-   limit: 10
+  local: states
 });
 
 $('#bloodhound .typeahead').typeahead({
   hint: true,
   highlight: true,
-  minLength: 1,
-  selectable: 'Typeahead-selectable',
-  limit: 10
+  minLength: 1
 },
 {
   name: 'states',
-  source: states
+  source: states,
+  limit: 10,
+  
 });
 
 });
