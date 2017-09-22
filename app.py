456455647d2dc7e920f56eb98a2259220c432b81
Greg <gkmarsella@gmail.com>
2017-09-22 16:08:40 -0700

    redirect back to home on error

--- app.py
+++ app.py
@@ -372,7 +372,7 @@ def results():
     except KeyError:
 
         flash('Please try again!')
-        return redirect(url_for('search'))
+        return redirect(url_for('home'))
 
 
 
@@ -458,7 +458,7 @@ def results():
     except KeyError:
 
         flash('Please try again!')
-        return redirect(url_for('search'))
+        return redirect(url_for('home'))
 
 
 
